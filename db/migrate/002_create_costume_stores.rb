4e1badf320a351de8c0dfa9de514d1b7e673928f
Sharmina Akhtar <sakht318@gmail.com>
2018-03-01 18:21:47 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,8 +5,11 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     create_table :costume_stores do |s|
       s.string :name
       s.string :location
-      s.integer :size
-      c.string :image_url
+      s.integer :costume_inventory
+      c.integer :num_of_employees
+      c.boolean :still_in_business
+      c.datetime :opening_time
+      c.datetime :closing_time
     end
   end
 end
