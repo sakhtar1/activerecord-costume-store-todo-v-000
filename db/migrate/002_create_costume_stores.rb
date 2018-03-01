e9b05c10ae91792cd1d6f9cf530fd1dfc86498f4
Sharmina Akhtar <sakht318@gmail.com>
2018-03-01 19:00:41 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |s|
+      s.string :name
+      s.string :location
+      s.integer :costume_inventory
+      s.integer :num_of_employees
+      s.boolean :still_in_business
+      s.datetime :opening_time
+      s.datetime :closing_time    
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :haunted_houses do |h|
+      h.string :name
+      h.string :location
+      h.string :theme
+      h.float :price
+      h.boolean :family_friendly
+      h.datetime :opening_date
+      h.datetime :closing_date
+      h.string :description
+    end
+  end
+end
