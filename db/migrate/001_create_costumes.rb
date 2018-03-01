6e5d23a334cd004270948667c15df4a9b5566446
Sharmina Akhtar <sakht318@gmail.com>
2018-03-01 18:16:22 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,7 +10,7 @@
 
 class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
-    create_table :costumes do |c|
+    create_table :costume do |c|
       c.string :name
       c.integer :price
       c.integer :size
