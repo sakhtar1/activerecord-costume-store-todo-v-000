9b185695e470a5294fc18f456febdded88a1fb1b
Sharmina Akhtar <sakht318@gmail.com>
2018-03-01 18:58:37 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -24,11 +24,9 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
-    t.string "size"
+    t.integer "price"
+    t.integer "size"
     t.string "image_url"
-    t.float "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "haunted_houses", force: :cascade do |t|
