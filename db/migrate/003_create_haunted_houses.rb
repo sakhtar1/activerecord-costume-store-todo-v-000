d4d992a46788c21b98fa79b905c228fb6f871d8a
Sharmina Akhtar <sakht318@gmail.com>
2018-03-01 18:22:18 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :haunted_houses do |s|
+      s.string :name
+      s.string :location
+      s.integer :costume_inventory
+      c.integer :num_of_employees
+      c.boolean :still_in_business
+      c.datetime :opening_time
+      c.datetime :closing_time    
+    end
+  end
+end
