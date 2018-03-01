3283d4c38b4df933d08a16bdf633b453f248c9bb
Sharmina Akhtar <sakht318@gmail.com>
2018-03-01 18:58:50 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
@@ -27,17 +27,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "price"
     t.integer "size"
     t.string "image_url"
-  end
-
-  create_table "haunted_houses", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "theme"
-    t.float "price"
-    t.boolean "family_friendly"
-    t.datetime "opening_date"
-    t.datetime "closing_date"
-    t.string "get_description"
   end
 
 end
